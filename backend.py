a259a2e7668df763bca426964db9369e1e0b9818
philiplessner <philiplessner@gmail.com>
2016-06-23 15:03:47 +0000

    initial commit

---
+++ backend.py
@@ -0,0 +1,81 @@
+import json
+from io import StringIO
+from typing import List, Dict, Any
+import ui
+import requests
+from toolz import pluck
+import pytablewriter
+
+
+BASE_PATENT ='http://www.patentsview.org/api/patents/query'
+BASE_ASSIGNEE = 'http://www.patentsview.org/api/assignees/query'
+
+
+def make_query(query: Dict[str, Any], fields: List[str], options: Dict[str, Any]) -> str:
+    payload = ''.join(['q=', json.dumps(query),
+                       '&f=', json.dumps(fields),
+                       '&o=', json.dumps(options)])
+    return payload
+     
+
+def get_info(payload: str, base=BASE_PATENT) -> Any:
+    r = requests.get(BASE_PATENT, params=payload)
+    if r.status_code == requests.codes.ok:
+        return r
+    else:
+        r.raise_for_status()
+    
+
+def get_output(fields: List[str], response: Any) -> List[str]:
+    patents = response['patents']
+    return list(pluck(fields, patents))
+    
+
+def formated_output(fields: List[str], raw_output: List[str]) -> str:
+    writer = pytablewriter.HtmlTableWriter()
+    html_begin = '<!DOCTYPE HTML>\n<html>\n<body>\n'
+    html_end = '\n</body>\n</html>'
+    writer.table_name = 'Query Output\n'
+    writer.header_list = fields
+    writer.value_matrix = raw_output
+    with StringIO() as f:
+        f.write(html_begin)
+        writer.stream = f
+        writer.write_table()
+        f.write(html_end)
+        html_text = f.getvalue()
+    return html_text
+    
+    
+def input2query(queryfield: str, condition: str, value: str) -> Dict[str, Dict[str, str]]:
+    return {condition:{queryfield:value}}
+
+
+def join_queries(queries, join_condition='_and'):
+    return {join_condition:queries}
+
+
+if __name__ == '__main__':
+#    query = {'_and': [{'_gte':{'patent_date':'2014-04-01'}}, {'_contains':{'assignee_organization':'KEMET'}}]}
+    query1 = input2query('assignee_organization', '_contains', 'KEMET')
+    query2 = input2query('patent_date', '_gte', '2014-04-01')
+    query = join_queries([query1, query2], join_condition='_and')
+    print(query)
+    fields = ['patent_number', 'patent_title', 'patent_date']
+    options = {'per_page':50}
+    payload = make_query(query, fields, options)
+    print('Query String\n', payload, '\n')
+    r = get_info(payload)
+    print('Encoded URL\n', r.url, '\n')
+    print('Status Code\n', r.status_code, '\n')
+    print('****Response****', '\n', r.json(), '\n\n')
+    raw_output = get_output(fields, r.json())
+    html_text = formated_output(fields, raw_output)
+    v = ui.View()
+    v.name = 'Test Web'
+    web = ui.WebView()
+    v.add_subview(web)
+    web.height = 1024
+    web.width = 720
+    web.load_html(html_text)
+    v.present('fullscreen')
---
+++ frontend_input.py
@@ -0,0 +1,186 @@
+import json
+import ui
+        
+             
+class BuildQuery(object):
+    def __init__(self):
+        self.v = ui.load_view('frontend_input')
+        qfds = ui.ListDataSource(items=['appcit_app_number',
+                                        'appcit_category',
+                                        'appcit_date',
+                                        'appcit_kind',
+                                        'appcit_sequence',
+                                        'app_country',
+                                        'app_date',
+                                        'app_number',
+                                        'app_type',
+                                        'assignee_city',
+                                        'assignee_country',
+                                        'assignee_first_name',
+                                        'assignee_first_seen_date',
+                                        'assignee_id',
+                                        'assignee_last_name',
+                                        'assignee_last_seen_date',
+                                        'assignee_lastknown_city',
+                                        'assignee_lastknown_country',
+                                        'assignee_lastknown_latitude',
+                                        'assignee_lastknown_location_id',
+                                        'assignee_lastknown_longitude',
+                                        'assignee_lastknown_state',
+                                        'assignee_latitude',
+                                        'assignee_location_id',
+                                        'assignee_longitude',
+                                        'assignee_organization',
+                                        'assignee_sequence',
+                                        'assignee_state',
+                                        'assignee_total_num_patents',
+                                        'assignee_type',
+                                        'cited_patent_category',
+                                        'cited_patent_date',
+                                        'cited_patent_kind',
+                                        'cited_patent_number',
+                                        'cited_patent_sequence',
+                                        'cited_patent_title',
+                                        'citedby_patent_category',
+                                        'citedby_patent_date',
+                                        'citedby_patent_kind',
+                                        'citedby_patent_number',
+                                        'citedby_patent_title',
+                                        'cpc_category',
+                                        'cpc_first_seen_date',
+                                        'cpc_group_id',
+                                        'cpc_group_title',
+                                        'cpc_last_seen_date',
+                                        'cpc_section_id',
+                                        'cpc_sequence',
+                                        'cpc_subgroup_id',
+                                        'cpc_subgroup_title',
+                                        'cpc_subsection_id',
+                                        'cpc_subsection_title',
+                                        'cpc_total_num_assignees',
+                                        'cpc_total_num_inventors',
+                                        'cpc_total_num_patents',
+                                        'inventor_city',
+                                        'inventor_country',
+                                        'inventor_first_name',
+                                        'inventor_first_seen_date',
+                                        'inventor_id',
+                                        'inventor_last_name',
+                                        'inventor_last_seen_date',
+                                        'inventor_lastknown_city',
+                                        'inventor_lastknown_country',
+                                        'inventor_lastknown_latitude',
+                                        'inventor_lastknown_location_id',
+                                        'inventor_lastknown_longitude',
+                                        'inventor_lastknown_state',
+                                        'inventor_latitude',
+                                        'inventor_location_id',
+                                        'inventor_longitude',
+                                        'inventor_sequence',
+                                        'inventor_state',
+                                        'inventor_total_num_patents',
+                                        'ipc_action_date',
+                                        'ipc_class',
+                                        'ipc_classification_data_source',
+                                        'ipc_classification_value',
+                                        'ipc_first_seen_date',
+                                        'ipc_last_seen_date',
+                                        'ipc_main_group',
+                                        'ipc_section',
+                                        'ipc_sequence',
+                                        'ipc_subclass',
+                                        'ipc_subgroup',
+                                        'ipc_symbol_position',
+                                        'ipc_total_num_assignees',
+                                        'ipc_total_num_inventors',
+                                        'ipc_version_indicator',
+                                        'nber_category_id',
+                                        'nber_category_title',
+                                        'nber_first_seen_date',
+                                        'nber_last_seen_date',
+                                        'nber_subcategory_id',
+                                        'nber_subcategory_title',
+                                        'nber_total_num_assignees',
+                                        'nber_total_num_inventors',
+                                        'nber_total_num_patents',
+                                        'patent_abstract',
+                                        'patent_average_processing_time',
+                                        'patent_date',
+                                        'patent_firstnamed_assignee_city',
+                                        'patent_firstnamed_assignee_country',
+                                        'patent_firstnamed_assignee_id',
+                                        'patent_firstnamed_assignee_latitude',
+                                        'patent_firstnamed_assignee_location_id',
+                                        'patent_firstnamed_assignee_longitude',
+                                        'patent_firstnamed_assignee_state',
+                                        'patent_firstnamed_inventor_city',
+                                        'patent_firstnamed_inventor_country',
+                                        'patent_firstnamed_inventor_id',
+                                        'patent_firstnamed_inventor_latitude',
+                                        'patent_firstnamed_inventor_location_id',
+                                        'patent_firstnamed_inventor_longitude',
+                                        'patent_firstnamed_inventor_state',
+                                        'patent_kind',
+                                        'patent_num_cited_by_us_patents',
+                                        'patent_num_claims',
+                                        'patent_num_combined_citations',
+                                        'patent_num_foreign_citations',
+                                        'patent_num_us_application_citations',
+                                        'patent_num_us_patent_citations',
+                                        'patent_number',
+                                        'patent_processing_time',
+                                        'patent_title',
+                                        'patent_type',
+                                        'patent_year',
+                                        'rawinventor_first_name',
+                                        'rawinventor_last_name',
+                                        'uspc_first_seen_date',
+                                        'uspc_last_seen_date',
+                                        'uspc_mainclass_id',
+                                        'uspc_mainclass_title',
+                                        'uspc_sequence',
+                                        'uspc_subclass_id',
+                                        'uspc_subclass_title',
+                                        'uspc_total_num_assignees',
+                                        'uspc_total_num_inventors',
+                                        'uspc_total_num_patents'
+                                       ])
+        
+        qf = self.v['queryfields']
+        qf.data_source = qf.delegate = qfds
+        qfds.action = self.qfds_action
+        qf.reload_data()
+        cods = ui.ListDataSource(items=['_eq',
+                                        '_neq',
+                                        '_gt',
+                                        '_gte',
+                                        '_lt',
+                                        '_lte',
+                                        '_begins',
+                                        '_contains',
+                                        '_text_all',
+                                        '_text_any',
+                                        '_text_phrase'
+                                        '_not',
+                                        '_and',
+                                        '_or'
+                                       ])
+        co = self.v['comparisons']
+        co.data_source = co.delegate = cods
+        cods.action = self.cods_action
+        co.reload_data()
+        self.v['btnadd2qry'].action = self.btnadd2qry_action
+        self.v.present('panel')
+        
+    def qfds_action(self, sender):
+        self.v['lblquery'].text = sender.items[sender.selected_row]
+    
+    def cods_action(self, sender):
+        self.v['lblcomparison'].text = sender.items[sender.selected_row]
+        
+    def btnadd2qry_action(self, sender):
+        queryd = {self.v['lblquery'].text: self.v['txtvalue'].text}
+        self.v['txtvquery'].text = json.dumps(queryd)
+
+
+BuildQuery()
