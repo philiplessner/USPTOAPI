4e31ad27559f354b5358f8226f28f01185e2a84a
philiplessner <philiplessner@gmail.com>
2016-07-03 12:12:04 +0000

    changed title of view and fullscreen to slideover

--- outputviewcontroller.py
+++ outputviewcontroller.py
@@ -3,10 +3,10 @@ import ui
 
 def viewoutput(html_text: str) -> None:
     v = ui.View()
-    v.name = 'Test Web'
+    v.name = 'USPTO Query Output'
     web = ui.WebView()
     v.add_subview(web)
     web.height = 1024
     web.width = 720
     web.load_html(html_text)
-    v.present('fullscreen')
+    v.present('panel')
