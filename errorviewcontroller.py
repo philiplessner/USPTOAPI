23a979e5b6e38340f6246e916fc29dec3901ae39
philiplessner <philiplessner@gmail.com>
2016-07-04 14:28:42 +0000

    added noqueryfields_dialog

--- errorviewcontroller.py
+++ errorviewcontroller.py
@@ -1,3 +1,4 @@
+import sys
 import ui
 import console
 import clipboard
@@ -17,4 +18,13 @@ def novaluesreturned_dialog(payload):
                            payload, 
                            'Copy to Clipboard')
     if button == 1:
-        clipboard.set(payload)    
+        clipboard.set(payload)
+        
+
+@ui.in_background
+def noqueryfields_dialog():
+    button = console.alert('Query Must Have At Least One Field', 
+                           '', 
+                           'OK',
+                           hide_cancel_button=True)
+    sys.exit(1)
